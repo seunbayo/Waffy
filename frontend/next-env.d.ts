b4b7220dd484df4aa2fc07ccb8594deaf29b72c2
seunbayo <seunbayo83@gmail.com>
2022-08-27 21:26:05 +0100

    trying

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+
+
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  trailingSlash: true,
+}
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
