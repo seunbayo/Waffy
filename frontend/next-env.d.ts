181e23e2f990eefb1b665ff274e0789966abd731
seunbayo <seunbayo83@gmail.com>
2022-08-27 21:30:19 +0100

    fixed

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -3,12 +3,5 @@
 
 
 
-module.exports = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  trailingSlash: true,
-}
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
