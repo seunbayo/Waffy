889f9a7dde20417c0a00053631450070ad4fd72a
seunbayo <seunbayo83@gmail.com>
2022-08-27 21:43:44 +0100

    try

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,11 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  trailingSlash: true,
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
